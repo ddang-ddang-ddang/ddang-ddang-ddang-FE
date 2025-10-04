a2011e3371d0a67f4892b434fcf57faac81eb712
chaeapark <yunapark2002@gmail.com>
2025-10-05 00:54:04 +0900

    feat: Navbar 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,10 @@
 export default function Main() {
   return (
-  <div>
-    <div className="text-yellow-400">Main 페이지입니다.</div>
-  </div>);
+    <div>
+      {/* 본문 */}
+      <div className="text-yellow-400 mt-10 text-center text-2xl">
+        Main 페이지입니다.
+      </div>
+    </div>
+  );
 }
