dd7a6593873900874917766ffbe80d0df5162e58
ChaeaPark <161542638+ChaeaPark@users.noreply.github.com>
2025-10-09 16:35:15 +0900

    ✨ 첫 재판 화면 UI 구현 (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,11 @@ import Main from "@/pages/Main";
 import MyPage from "@/pages/MyPage";
 import Navbar from "@/components/layout/Navbar";
 import Login from "./pages/login/LoginPage";
+import FirstTrialStart from "@/pages/FirstTrial/FirstTrialStart";
+import FirstTrialSubmit from "@/pages/FirstTrial/FirstTrialSubmit";
+import FirstTrialLoading from "@/pages/FirstTrial/FirstTrialLoading";
+import FirstTrialResult from "@/pages/FirstTrial/FirstTrialResult";
+import FirstTrialJudge from "./pages/FirstTrial/FirstTrialJudge";
 
 function App() {
   return (
@@ -15,6 +20,11 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/firsttrial/start" element={<FirstTrialStart />} />
+        <Route path="/firsttrial/submit" element={<FirstTrialSubmit />} />
+        <Route path="/first-trial/loading" element={<FirstTrialLoading />} />
+        <Route path="/first-trial/result" element={<FirstTrialResult />} />
+        <Route path="/ai-judge" element={<FirstTrialJudge />} />
       </Routes>
     </>
   );
