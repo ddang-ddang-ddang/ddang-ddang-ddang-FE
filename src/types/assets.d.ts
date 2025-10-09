a1b13ef03f003f89bf0a2cd6cceebb77d5bb93c2
chan000518 <wkdvnddl050701@gmail.com>
2025-10-09 17:47:02 +0900

    ⚡️ ➕ svg 포함 다양한 파일 지원

--- src/types/assets.d.ts
+++ src/types/assets.d.ts
@@ -3,3 +3,34 @@ declare module "*.svg" {
   const content: string;
   export default content;
 }
+
+// Support importing SVGs as React components via `?react` (vite-plugin-svgr)
+declare module "*.svg?react" {
+  import * as React from "react";
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string }
+  >;
+  export default ReactComponent;
+}
+
+// Common raster/image assets
+declare module "*.gif" {
+  const src: string;
+  export default src;
+}
+declare module "*.png" {
+  const src: string;
+  export default src;
+}
+declare module "*.jpg" {
+  const src: string;
+  export default src;
+}
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
+}
+declare module "*.webp" {
+  const src: string;
+  export default src;
+}
